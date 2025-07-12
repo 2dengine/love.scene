e27dc201343825806f1acc87085ed01bf1232afb
2dengine <support@2dengine.com>
2025-07-12 15:32:07 +0300

    MIT

--- scene/sprite.lua
+++ scene/sprite.lua
@@ -1,3 +1,30 @@
+--[[
+This file is part of the "love.scene" library.
+https://2dengine.com/doc/scene.html
+
+MIT License
+
+Copyright (c) 2020 2dengine LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
 --- Sprites are nodes in the scene which can be translated, scaled or rotated.
 -- Each sprite is assigned a "drawable" graphic, usually an image, quad or text.
 -- Sprites can also be modulated by changing their color, alpha value and blending mode.
@@ -5,6 +32,7 @@
 -- @alias sprite
 -- @inherit node
 local sprite = {}
+
 local reg = debug.getregistry()
 reg.Sprite = sprite
 
